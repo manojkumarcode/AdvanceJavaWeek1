e7aa31e72ff7a370cbaa899abc3bf17ad0832b29
Manoj Kumar <manoj.er.kumar@gmail.com>
2021-09-18 23:02:01 +0530

    CopyOnWriteArrayList solves the concurrentModificationException

--- src/main/java/com/manoj/advance/java/week1/concurrentapi/ConcurrentModificationExceptioDemo.java
+++ src/main/java/com/manoj/advance/java/week1/concurrentapi/ConcurrentModificationExceptioDemo.java
@@ -2,10 +2,11 @@ package com.manoj.advance.java.week1.concurrentapi;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ConcurrentModificationExceptioDemo {
     public static void main(String[] args) {
-        List<String> arrayList = new ArrayList<>();
+        List<String> arrayList = new CopyOnWriteArrayList<>();
         arrayList.add("abc");
         arrayList.add("bcd");
 
